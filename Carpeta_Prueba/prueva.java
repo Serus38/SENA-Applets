355a1322fea2f9db8ec023c2401d2fc44bbc2f38
Serus38 <matambita08@gmail.com>
2026-05-20 20:36:52 -0500

    Add comment for heatmap project update

--- Carpeta_Prueba/prueva.java
+++ Carpeta_Prueba/prueva.java
@@ -3,4 +3,5 @@ package Carpeta_Prueba;
 public class prueva {
     //solo para el heatmap 1
     //Esta vez sera para el heatmap 2
+    //no pude subir a tiempo el proyecto asi que hago esto jaja
 }
