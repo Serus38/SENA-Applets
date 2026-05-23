9d0a12ec69a7ddc5a1679fdc21f5b726d6b611a3
Serus38 <matambita08@gmail.com>
2026-05-22 21:15:35 -0500

    ...

--- Carpeta_Prueba/prueva.java
+++ Carpeta_Prueba/prueva.java
@@ -5,4 +5,5 @@ public class prueva {
     //Esta vez sera para el heatmap 2
     //no pude subir a tiempo el proyecto asi que hago esto jaja
     //No sé que ponerle a mi dashbord, que estres jaja, ahora est ese va convertir en una bitacora de mis avances, espero que me sirva para organizarme mejor, aunque no se si lo logre jajaja xD
+    //hoy si pude subir el proyecto, aunque no se si me dio tiempo de hacer todo lo que queria, pero bueno, al menos ya esta subido, ahora a seguir trabajando en otro proyecto para mejorarlo cada vez mas
 }
