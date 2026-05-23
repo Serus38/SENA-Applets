0fecd96a8d38c2cca83693dc68f1c2117658caa0
Serus38 <matambita08@gmail.com>
2026-05-22 21:17:01 -0500

    terminacion de heatmap en este proyecto

--- Carpeta_Prueba/prueva.java
+++ Carpeta_Prueba/prueva.java
@@ -6,4 +6,5 @@ public class prueva {
     //no pude subir a tiempo el proyecto asi que hago esto jaja
     //No sé que ponerle a mi dashbord, que estres jaja, ahora est ese va convertir en una bitacora de mis avances, espero que me sirva para organizarme mejor, aunque no se si lo logre jajaja xD
     //hoy si pude subir el proyecto, aunque no se si me dio tiempo de hacer todo lo que queria, pero bueno, al menos ya esta subido, ahora a seguir trabajando en otro proyecto para mejorarlo cada vez mas
+    //Dejare este proyecto y continuare el heatmap en otro para dejar este en paz.
 }
